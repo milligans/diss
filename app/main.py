a6596bbe4b429bfb676b8678d6773842d5844c4d
korova_blue@hotmail.com <73536360+milligans@users.noreply.github.com>
2021-10-24 18:14:45 +0100

    made a better contact page, started added contact form

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from jinja2 import Template
 from flask import render_template, url_for, request, redirect, flash
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from flask_bootstrap import Bootstrap
+from app.forms import ContactForm
 
 app= Flask(__name__)
-@app.route('/')
+Bootstrap(app)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form=ContactForm(request.form)
     return render_template("index.html")
