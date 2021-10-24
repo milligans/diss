70d0c6433248b88dfde67ed35b0340e7d3f5c686
korova_blue@hotmail.com <73536360+milligans@users.noreply.github.com>
2021-10-24 18:14:59 +0100

    made a better contact page, started added contact form

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from wtforms import Form, BooleanField, StringField, PasswordField, validators, TextField
+
+
+class ContactForm(Form):
+    name = StringField('Name', [validators.Length(min=4, max=120)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    message = TextField('Message')
+
+
